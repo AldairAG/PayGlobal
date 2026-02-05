35482c14daafafb9569b12816b5ecf6f6773aad3
AldairAG <alagga02@gmail.com>
2026-02-05 14:31:39 -0600

    chore: update AdminLayout and UserLayout to include Outlet component

--- frontend/src/layout/AdminLayout.tsx
+++ frontend/src/layout/AdminLayout.tsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+
 export const AdminLayout = () => {
     return (
         <div>
             <h1>Admin Layout</h1>
+
+            <div>
+                <Outlet/>
+            </div>
         </div>
     );
 }
--- frontend/src/layout/UserLayout.tsx
+++ frontend/src/layout/UserLayout.tsx
@@ -1,8 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 export const UserLayout = () => {
     return (
         <div>
             <h1>User Layout</h1>
-            
+            <div>
+                <Outlet/>
+            </div>
         </div>
     );
 }
