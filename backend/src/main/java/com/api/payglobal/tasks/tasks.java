ffbd77d3b5d4f23b80f4bee2c16d8379985414af
AldairAG <alagga02@gmail.com>
2026-07-23 14:15:33 -0600

    Prueba de reinicio de servidor

--- backend/src/main/java/com/api/payglobal/tasks/tasks.java
+++ backend/src/main/java/com/api/payglobal/tasks/tasks.java
@@ -1,5 +1,5 @@
 package com.api.payglobal.tasks;
-
+//prueba de reinicio de servidor
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
